43bb222eae5e4f6f3d55a7d6e029e497e955f431
Michprosal003 <95501615+Michprosal003@users.noreply.github.com>
2022-02-06 12:35:24 -0500

    Add files via upload

---
+++ portal.php
@@ -0,0 +1,120 @@
+
+
+<!--metodo para mejorar la memoria no se requiere ningun esfuerzo-->
+
+<!-- 
+
+1: body mpmbdjaf
+2:container wpdj
+3:container .card pc 
+4:container .card .face wht 
+5:container .card .face.face1 pbdjazt 
+6:container .card:hover .face.face1 bt 
+7: container .card .face.face1 .content ot 
+8: container .card:hover .face.face1 .content o
+9: container .card .face.face1 .content img mh 
+10: container .card .face.face2 pbdjapbbt 
+11:  container .card:hover .face.face2 t 
+12:   container .card .face.face2 .content p mp 
+13: container .card .face.face2 .content a mdtfpbc 
+14: container .card .face.face2 .content a: hover bc 
+
+-->
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <div class="college">
+    <h1>Escoga una de las mejores universidades de Panama</h1>
+    
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="container">
+<div class="card">
+<div class="face face1">
+<div class="content">
+<img src="imagenes/up.jpg">
+</div>
+</div>
+
+
+<div class="face face2">
+<div class="content">
+<p>Universidad de Panama</p>
+<a href="https://www.up.ac.pa/">Informacion</a>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+<div class="card">
+<div class="face face1">
+<div class="content">
+<img src="imagenes/udelas.jpeg">
+</div>
+</div>
+
+
+<div class="face face2">
+<div class="content">
+<p>Universidad Especializada de las americas</p>
+<a href="http://www.udelas.ac.pa/">Informacion</a>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="card">
+<div class="face face1">
+<div class="content">
+<img src="imagenes/uip.jpg">
+</div>
+</div>
+
+
+<div class="face face2">
+<div class="content">
+<p>Universidad Interamericana de Panama</p>
+<a href="https://uip.edu.pa/">Informacion</a>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
